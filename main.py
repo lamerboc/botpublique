afa7d1d48c74f64fdbe3e4a1b743fac9314dc99c
Liekvahan <81919992+lamerboc@users.noreply.github.com>
2021-10-16 20:25:05 +0200

    Create main.py
    Mise à disposition la commande warn et coucou
    Si le bot ne marche pas randez vous en message priver 
    Liek#2381

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import json
+
+bot = commands.Bot(command_prefix = "!", description = "Robot discord")
+
+@bot.event()
+async def on_ready()
+    print("Je suis connecter sur discord | Prêt a etre utiliser !")
+    
+@bot.command()
+async def coucou(ctx):
+    await ctx.send("Coucou !")
+
+@bot.command()
+@commands.has_permissions(administrateur=True)
+async def warn(ctx, user, *reason)
+    await ctx.send(f"Je viens de avertir {user} pour la raison : {reason}")
+
+bot.run("LOGIN.TOKEN.BOT.DISCORD")
